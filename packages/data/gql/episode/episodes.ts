61e085baca0c207d8d1a8de9c3497386fe1d6112
Eric Bright <ericbdev@gmail.com>
2021-10-09 19:31:14 -0400

    Ahaha it does a graph

--- packages/data/gql/episode/episodes.ts
+++ packages/data/gql/episode/episodes.ts
@@ -1,8 +1,10 @@
 import { gql } from '@apollo/client';
 import { CHARACTER_FIELDS } from '@wiki/gql/character/fragments';
+import { EPISODE_FIELDS } from './fragments';
 
 export const GET_EPISODES = gql`
   ${CHARACTER_FIELDS}
+  ${EPISODE_FIELDS}
   query GetEpisodes($page: Int, $filter: FilterEpisode) {
     episodes(page: $page, filter: $filter) {
       info {
@@ -12,10 +14,6 @@ export const GET_EPISODES = gql`
         prev
       }
       results {
-        id
-        episode
-        name
-        air_date
         characters {
           ...CharacterFields
         }
